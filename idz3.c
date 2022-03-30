c57fd223de9342ca002b3d0c5a4e8a7732926178
Ismailovmr <92060997+Fuji12@users.noreply.github.com>
2022-03-30 23:03:00 +0300

    update

--- idz3.c
+++ idz3.c
@@ -2,24 +2,39 @@
 
 int main()
 {
-    char line[75];
-    int n = 1, e = 0;
-    FILE* in = fopen("input.txt", "r");
-    fgets(line, 75, in);
-    for (int i = 0; line[i]; ++i) {
-        if ((line[i] != ' ') && (line[i] != '\n')) {
-            ++e;
-        }
+    int matrix[15][2];// для создания матрицы
+    int i,j;//для персчёта эл-тов
+    int m, l;//счётчики вершин
+    int n = 1, e = 0;//итоговый счётчик вершин и рёбер
+    int max=0;
+    FILE *file = fopen("input.txt", "r");//работа с файлом
+    for (i = 0; i < 6 && !feof(file); i++){
+    for (j = 0; j < 2 && !feof(file); j++){
+      fscanf(file, "%d", &matrix[i][j]);
+      printf("%d ", matrix[i][j]);
     }
-    while (fgets(line, 75, in)) {
-        ++n;
+    putchar('\n');
+  }
+  for (i = 0; i < 6; i++) {//подсчёт вершин
+
+    if(matrix[i][0] > max){
+        max = matrix[i][0];
     }
-    if (e > ((n - 1) * (n - 2) / 2)) {
+
+    if(matrix[i][1] > max){
+        max = matrix[i][1];
+    }
+    
+  } 
+  max++;
+  n=i;
+  printf("%i\n", max);
+  if (n > ((max - 1) * (max - 2) / 2)) {//формула для вычисление связности графа
         printf("This is a connected graph");
     }
     else {
         printf("This is a disconnected graph");
     }
-    fclose(in);
-    return 0;
+    fclose(file);
+    return 0;    
 }
