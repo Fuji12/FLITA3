1d5b8c1e8d4ec6edd3dd765d3bd462fded55ac49
Ismailovmr <92060997+Fuji12@users.noreply.github.com>
2022-04-05 18:13:34 +0300

    update

--- idz3.c
+++ idz3.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
+#define graf_height 10
+#define graf_width 2
+
 int main()
 {
-    int matrix[15][2];// для создания матрицы
+    int matrix[graf_height][graf_width];// для создания матрицы
     int i,j;//для персчёта эл-тов
     int m, l;//счётчики вершин
     int n = 1, e = 0;//итоговый счётчик вершин и рёбер
     int max=0;
     FILE *file = fopen("input.txt", "r");//работа с файлом
-    for (i = 0; i < 6 && !feof(file); i++){
-    for (j = 0; j < 2 && !feof(file); j++){
+    for (i = 0; i < graf_height && !feof(file); i++){
+    for (j = 0; j < graf_width && !feof(file); j++){
       fscanf(file, "%d", &matrix[i][j]);
       printf("%d ", matrix[i][j]);
     }
     putchar('\n');
   }
-  for (i = 0; i < 6; i++) {//подсчёт вершин
+  for (i = 0; i < graf_height; i++) {//подсчёт вершин
 
     if(matrix[i][0] > max){
         max = matrix[i][0];
@@ -29,7 +32,7 @@ int main()
   max++;
   n=i;
   printf("%i\n", max);
-  if (n > ((max - 1) * (max - 2) / 2)) {//формула для вычисление связности графа
+  if (max < ((n - 1) * (n - 2) / 2)) {//формула для вычисление связности графа
         printf("This is a connected graph");
     }
     else {
